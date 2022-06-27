02ec04ba0dcb83bec7af465ae671a893ec51df70
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-06-27 03:06:45 +0000

    Fix Windows by using .exe
    Windows doesn't handle well when we symlink commands in tarball
    especially with pipe.
    
    Instead, .exe installer has better scripts so that we use it.

--- __tests__/download-util.test.ts
+++ __tests__/download-util.test.ts
@@ -1,13 +1,14 @@
-import {downloadAndExtract} from '../src/download-util'
+import {download} from '../src/download-util'
 import {expect, test, beforeAll} from '@jest/globals'
 
 test.each([
-  ['linux-x64', undefined],
-  ['linux-x64', '0.1.0']
+  ['linux-x64.tar.gz', undefined],
+  ['linux-x64.tar.gz', '0.1.0'],
+  ['x64.exe', undefined]
 ])(
   'downloadAndExtract target=%s version=%s',
   async (target, version) => {
-    expect(await downloadAndExtract(target, version)).toBeDefined()
+    expect(await download(target, version)).toBeDefined()
   },
   60_000
 )
