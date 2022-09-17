97b9e5fe3893713fc3ca787f6c0a10f100ac2aea
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-09-17 03:28:02 +0000

    Remove cache and integration test options
    We change mind that these options are not needed to expose.
    
    This commit enables cache always and let the environment variable decide
    whether integration test is needed.

--- script.bash
+++ script.bash
@@ -2,17 +2,9 @@
 set -xe
 
 : "${INPUT_SHELL_INSTALLER_URL:?"Provide the installer URL"}"
-: "${INPUT_USE_CACHE:?"Provide true or false"}"
-: "${INPUT_INSTALL_CLI_INTEGRATION_TEST:?"Provide true or false"}"
-
-if [ "$INPUT_USE_CACHE" == "true" ]; then
-  export AUTIFY_CLI_INSTALL_USE_CACHE=1
-fi
-if [ "$INPUT_INSTALL_CLI_INTEGRATION_TEST" == "true" ]; then
-  export AUTIFY_CLI_INTEGRATION_TEST_INSTALL=1
-fi
 
 cd "$RUNNER_TEMP"
+export AUTIFY_CLI_INSTALL_USE_CACHE=1
 curl -L "$INPUT_SHELL_INSTALLER_URL" | bash -xe
 
 cat "$RUNNER_TEMP/autify/path" >> "$GITHUB_PATH"
